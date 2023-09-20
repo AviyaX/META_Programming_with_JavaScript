e2095b9eb2ba72c3128deb2040d8c35dd6e8eec8
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-20 20:08:21 +0530

    Working with conditionals and loops - IF else

--- META.js
+++ META.js
@@ -98,3 +98,19 @@ switch(day) {
        //this block will run if no condition matches
        console.log('There is no such day');
 }
+
+// Working with conditionals and loops - IF else 
+for (var i = 1; i <= 10; i++) {
+    if(i == 1) {
+        console.log("Gold medal")
+    } else if (i == 2) {
+        console.log("Silver medal")
+    } else if (i == 3) {
+        console.log("Bronze medal")
+    } else {
+        //this block will run if no condition matches
+        console.log(i)
+    }
+}
+
+
